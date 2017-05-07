ada572a2a5a17e0710026612b2fb708b5197603b
Cyrille Chopelet <cyrille.chopelet@mines-nancy.org>
2017-05-07 15:30:52 +0200

    Init basic Scala integration

--- src/main/java/org/keyboardplaying/application/ExecutionController.java
+++ src/main/java/org/keyboardplaying/application/ExecutionController.java
@@ -1,5 +1,6 @@
 package org.keyboardplaying.application;
 
+import org.keyboardplaying.scala.ScalaTester;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ public class ExecutionController {
 
     @RequestMapping("/")
     public String index() {
+        new ScalaTester().runScala("Spring Boot");
         return "Greetings from Spring Boot!";
     }
 
---
+++ src/main/scala/org/keyboardplaying/scala/ScalaTester.scala
@@ -0,0 +1,12 @@
+package org.keyboardplaying.scala
+
+/**
+  * Just a basic test to ensure Scala is running properly.
+  *
+  * @author Cyrille Chopelet
+  */
+class ScalaTester {
+  def runScala(name: String) = {
+    println("Hello, " + name + "!")
+  }
+}
