3f10bfc4df4712b7cf36053d57a16e0f580b2b67
Cyrille Chopelet <cyrille.chopelet@cgi.com>
2017-06-09 17:02:01 +0200

    Init php dockerfile

---
+++ powerapi-demo-php-server/src/index.php
@@ -0,0 +1,7 @@
+<?php
+var_dump(phpversion("mongodb"));
+
+//$client = new MongoDB\Client("mongodb://localhost:27017");
+//echo $m->listDBs();
+//$db = $m->selectDB("people");
+//echo $db->getCollectionNames();
